b7c4ee6361cce7b66a01c329899827f3667eda29
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-10-05 10:44:07 +0200

    finnishing touches
    fixed #4

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
@@ -10,7 +13,34 @@ export class ResetPasswordPage implements OnInit {
     email: ['', Validators.compose([Validators.required, Validators.email])],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    public alertController: AlertController,
+    public router: Router,
+    public auth: AuthService
+  ) {}
+
+  async resetPassword() {
+    await this.auth.resetPassword(this.email.value);
+    this.resetPasswordForm.reset();
+    this.alertController
+      .create({
+        header: 'Überprüfe deine Mails',
+        message: 'Du solltest in kürze eine Email von uns bekommen.',
+        buttons: [
+          {
+            text: 'Zum login',
+            handler: () => {
+              this.router.navigateByUrl('/login');
+            },
+          },
+        ],
+      })
+      .then((alert) => alert.present());
+  }
+  get email() {
+    return this.resetPasswordForm.get('email');
+  }
 
   ngOnInit() {}
 }
