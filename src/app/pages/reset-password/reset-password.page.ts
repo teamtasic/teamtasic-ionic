46134acfd3b6eb0002a2635fe5910820cd272b2e
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-10-05 10:23:20 +0200

    UI changes for resetting password

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
 })
 export class ResetPasswordPage implements OnInit {
+  resetPasswordForm: FormGroup = this.fb.group({
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
