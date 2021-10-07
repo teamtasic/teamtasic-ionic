ed4822084072e9b8754281b31e2b7b26a468d3ee
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-10-07 11:10:22 +0200

    added prod configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAXt5cAyEXgLIwmX6ewiaHNGt7H1eMmgCU',
-    authDomain: 'teamtasic-dev.firebaseapp.com',
-    projectId: 'teamtasic-dev',
-    storageBucket: 'teamtasic-dev.appspot.com',
-    messagingSenderId: '967538874315',
-    appId: '1:967538874315:web:187a87c28493e42101c84e',
-    measurementId: 'G-L8LMJC5VLT',
+    apiKey: 'AIzaSyDlRN2F2oACwPKrJDKKab3NYWRFSvhZUmw',
+    authDomain: 'teamtasic.firebaseapp.com',
+    projectId: 'teamtasic',
+    storageBucket: 'teamtasic.appspot.com',
+    messagingSenderId: '1081225866059',
+    appId: '1:1081225866059:web:aae2a6c8b1b52672d796ce',
+    measurementId: 'G-JJ42RSJ0YZ',
   },
 };
