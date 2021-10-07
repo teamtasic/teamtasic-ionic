be5b7317368bd7e22b5566f49380f50d9145e6ff
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-10-07 10:35:29 +0200

    fixed #34

--- src/app/pages/my-account/my-account.page.ts
+++ src/app/pages/my-account/my-account.page.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataRepositoryService } from 'src/app/services/data-repository.service';
 import { Clipboard } from '@capacitor/clipboard';
 import { Share } from '@capacitor/share';
+import { AuthService } from 'src/app/services/auth.service';
+import { NotificationService } from 'src/app/services/notification-service.service';
 
 @Component({
   selector: 'app-my-account',
@@ -10,17 +12,58 @@ import { Share } from '@capacitor/share';
   styleUrls: ['./my-account.page.scss'],
 })
 export class MyAccountPage implements OnInit {
-  constructor(public drs: DataRepositoryService, public fb: FormBuilder) {}
+  constructor(
+    public drs: DataRepositoryService,
+    public auth: AuthService,
+    public fb: FormBuilder,
+    public ns: NotificationService
+  ) {}
 
   userData: FormGroup = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
+    username: ['', Validators.required],
+    phoneNumber: ['', Validators.required],
+    address: ['', Validators.required],
+    zipcode: ['', Validators.required],
   });
+  _userData: FormGroup = this.userData;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.drs.currentUser.subscribe((user) => {
+      if (user) {
+        this.userData = this.fb.group({
+          username: [user.username, Validators.required],
+          phoneNumber: [user.phoneNumber, Validators.required],
+          address: [user.address, Validators.required],
+          zipcode: [user.zipcode, Validators.required],
+        });
+      }
+    });
+  }
+  async saveUserData() {
+    let user = await this.drs.currentUser.getValue();
+    if (user) {
+      try {
+        user.username = this.username.value;
+        user.phoneNumber = this.phoneNumber.value;
+        user.address = this.address.value;
+        user.zipcode = this.zipcode.value;
+        this.drs.currentUser.next(user);
+        await this.drs.updateUser();
+        this.ns.showToast('Deine Daten wurden gespeichert.');
+      } catch (error) {
+        this.ns.showToast(`Fehler beim Speichern der Daten: ${error}`);
+      }
+    }
+  }
 
   async copyUidToClipboard() {
-    await Clipboard.write({ string: this.drs.currentUser.getValue().uid });
+    try {
+      await Clipboard.write({ string: this.drs.currentUser.getValue().uid });
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+  async shareUid() {
     try {
       await Share.share({
         title: 'Meine Teamtasic User Id, füge mich deinem Team hinzu!',
@@ -32,5 +75,17 @@ export class MyAccountPage implements OnInit {
     } catch (error) {
       console.warn(error);
     }
+  }
+  get username() {
+    return this.userData.get('username');
+  }
+  get phoneNumber() {
+    return this.userData.get('phoneNumber');
+  }
+  get address() {
+    return this.userData.get('address');
+  }
+  get zipcode() {
+    return this.userData.get('zipcode');
   }
 }
