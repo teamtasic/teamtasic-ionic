d8a2480c9b2544cf2ecef4823240e8b211c437fc
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-11-10 23:47:19 +0100

    added user account selector component

---
+++ src/app/components/user-account-selector/user-account-selector.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-selector',
+  templateUrl: './user-account-selector.component.html',
+  styleUrls: ['./user-account-selector.component.scss'],
+})
+export class UserAccountSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
