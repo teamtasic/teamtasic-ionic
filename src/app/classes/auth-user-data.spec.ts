11f083ed06ecfb9664e70e9cd16425558dd5f890
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-11-14 19:35:04 +0100

    brougth auth user data to spec

--- src/app/classes/auth-user-data.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthUserData } from './auth-user-data';
-
-describe('AuthUserData', () => {
-  it('should create an instance', () => {
-    expect(new AuthUserData()).toBeTruthy();
-  });
-});
--- src/app/classes/auth-user-data.ts
+++ src/app/classes/auth-user-data.ts
@@ -2,36 +2,25 @@ import { DocumentReference } from '@angular/fire/firestore';
 
 export class AuthUserData {
   uid: string;
-  ref: DocumentReference;
 
   username: string;
   email: string;
-  // fields added 2021-10-06
   phoneNumber: string;
-  birthDate: string;
   address: string;
   zipcode: string;
 
-  memberships: Object[];
-
   constructor(
     uid: string,
-    ref: DocumentReference,
     username: string,
     email: string,
-    memberships: Object[],
     phoneNumber: string = '',
-    birthDate: string = '',
     address: string = '',
     zipcode: string = ''
   ) {
     this.uid = uid;
-    this.ref = ref;
     this.username = username;
     this.email = email;
-    this.memberships = memberships;
     this.phoneNumber = phoneNumber;
-    this.birthDate = birthDate;
     this.address = address;
     this.zipcode = zipcode;
   }
@@ -41,12 +30,9 @@ export class AuthUserData {
       const data = snapshot.data(options);
       return new AuthUserData(
         snapshot.id,
-        snapshot.ref,
         data.username,
         data.email,
-        data.memberships,
         data.phoneNumber,
-        data.birthDate,
         data.address,
         data.zipcode
       );
@@ -55,9 +41,7 @@ export class AuthUserData {
       return {
         username: data.username,
         email: data.email,
-        memberships: data.memberships,
         phoneNumber: data.phoneNumber,
-        birthDate: data.birthDate,
         address: data.address,
         zipcode: data.zipcode,
       };
