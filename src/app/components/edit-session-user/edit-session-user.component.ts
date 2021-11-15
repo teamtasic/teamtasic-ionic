2593d37e3fa33e8b63b5ba2889c9c948d9463035
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-11-15 18:01:26 +0100

    added edit session component

---
+++ src/app/components/edit-session-user/edit-session-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-session-user',
+  templateUrl: './edit-session-user.component.html',
+  styleUrls: ['./edit-session-user.component.scss'],
+})
+export class EditSessionUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
