40f02eb0d85bb4075af8c30d3c6c290b2eb15993
JesseB0rn <50794219+JesseB0rn@users.noreply.github.com>
2021-12-08 19:34:18 +0100

    settings for badges

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -19,6 +19,9 @@ const config: CapacitorConfig = {
       splashFullScreen: true,
       splashImmersive: true,
     },
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
   },
 };
 
